4839cc1f9b4cc0231248fc722babaf70db1f288e
Samuelsnd <samueldasilva9211@gmail.com>
2023-10-10 01:31:22 -0300

    adding the route create of controller

--- crud-spring/src/main/java/com/samuel/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/samuel/crudspring/controller/CourseController.java
@@ -3,9 +3,13 @@ package com.samuel.crudspring.controller;
 
 import java.util.List;
 
-
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.samuel.crudspring.Repository.CourseRepository;
@@ -26,5 +30,11 @@ public class CourseController {
     @GetMapping
     public List<Course> findAll() {
         return courseRepository.findAll();
+    }
+
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {
+        return courseRepository.save(course);
     }
 }
