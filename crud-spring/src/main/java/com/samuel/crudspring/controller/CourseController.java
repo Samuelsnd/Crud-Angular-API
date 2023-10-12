1443668124ae50d4d699b8a18a304bc6c8265f6f
Samuelsnd <samueldasilva9211@gmail.com>
2023-10-11 23:29:29 -0300

    adding the method of get through id

--- crud-spring/src/main/java/com/samuel/crudspring/controller/CourseController.java
+++ crud-spring/src/main/java/com/samuel/crudspring/controller/CourseController.java
@@ -6,9 +6,11 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,8 +30,15 @@ public class CourseController {
 
     //@RequestMapping(method = RequestMethod.GET)
     @GetMapping
+    @ResponseBody
     public List<Course> findAll() {
         return courseRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> findById(@PathVariable Long id) {
+        return courseRepository.findById(id)
+        .map(record -> ResponseEntity.ok(record)).orElse(ResponseEntity.notFound().build());
     }
 
     @PostMapping
